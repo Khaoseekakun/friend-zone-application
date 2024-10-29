3af87faf0e910e7a47b7695ff156df5f680ec5ff
Khao Seekakun <emitstudio13829@gmail.com>
2024-10-29 10:59:00 +0700

    fff

--- app/screen/SearchCategory.tsx
+++ app/screen/SearchCategory.tsx
@@ -1,47 +1,89 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, Button, TouchableOpacity, Platform, KeyboardAvoidingView } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, Platform, KeyboardAvoidingView, Image } from "react-native";
 import { NavigationProp, useNavigation } from "@react-navigation/native";
-import io, { Socket } from "socket.io-client";
 
 import { styled } from "nativewind";
-import { Ionicons } from "@expo/vector-icons";
-import { FlatList, TextInput } from "react-native-gesture-handler";
 import axios from "axios";
 import { HeaderApp } from "@/components/Header";
 import { Navigation } from "@/components/Navigation";
+import { LinearGradient } from "expo-linear-gradient";
+import { Ionicons } from "@expo/vector-icons";
 const StyledView = styled(View);
 const StyledText = styled(Text);
-const StyledTextInput = styled(TextInput);
-const StyledIonIcon = styled(Ionicons)
-
+const StyledImage = styled(Image);
+const FriendImage = require("../../assets/images/InApp/friends.jpg");
+const DjImage = require("../../assets/images/InApp/dj.jpg");
+const MusicImage = require("../../assets/images/InApp/musicband.jpg");
+const StyledIcon = styled(Ionicons);
 export default function Message() {
     const navigation = useNavigation<NavigationProp<any>>();
-    const [search, setSearch] = useState('');
-    const [searchloading, setSearchLoading] = useState(false);
-
-    const handlerMessageSearch = async() => {
-        setSearchLoading(true);
-        try {
-            const searchMessage = await axios.get(`http://49.231.43.37:3000/api/message`)
-        } catch (error) {
-
-        } finally {
-            setSearchLoading(false);
-        }
-    }
-
 
     return (
         <StyledView className="flex-1">
-            <HeaderApp />
             <KeyboardAvoidingView
                 style={{ flex: 1 }}
                 behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
             >
-                <StyledView className="flex-1 bg-white dark:bg-black">
-                </StyledView>
+                <LinearGradient
+                    colors={['#EB3834', '#69140F']}
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 1, y: 1 }}
+                    className="flex-1 justify-center items-center"
+                >
+                    <TouchableOpacity onPress={() => navigation.goBack()} className="absolute pt-[60] left-0 top-0 ml-4">
+                        <Ionicons name="chevron-back" size={24} color="white" />
+                    </TouchableOpacity>
+                    <StyledView className="w-10/12 rounded-full h-[100px] justify-center border-[2px] border-white shadow-md my-2">
+
+                        <StyledImage source={FriendImage} className="w-full rounded-full  h-[96px] justify-center " />
+
+                        <StyledView className="absolute w-full bg-black rounded-full opacity-40 h-[96px]" />
+
+                        <StyledView className="absolute self-center">
+                            <StyledIcon className="self-center text-center font-custom text-white" name="people" size={35} color="white" />
+                            <StyledText className="self-center text-center font-custom text-white text-2xl">เพื่อนเที่ยว</StyledText>
+                        </StyledView>
+
+                    </StyledView>
+
+                    <StyledView className="w-10/12 rounded-full h-[100px] justify-center border-[2px] border-white shadow-md my-2">
+
+                        <StyledImage source={DjImage} className="w-full rounded-full  h-[96px] justify-center " />
+
+                        <StyledView className="absolute w-full bg-black rounded-full opacity-[0.85] h-[96px]" />
+
+
+
+                        <StyledView className="absolute self-center">
+                            <StyledIcon className="self-center text-center font-custom text-white" name="hammer" size={25} color="white" />
+                            <StyledText className="self-center text-center font-custom text-white text-2xl">SOON</StyledText>
+                        </StyledView>
+
+                    </StyledView>
+
+                    <StyledView className="w-10/12 rounded-full h-[100px] justify-center border-[2px] border-white shadow-md my-2">
+
+                        <StyledImage source={MusicImage} className="w-full rounded-full  h-[96px] justify-center " />
+
+                        <StyledView className="absolute w-full bg-black rounded-full opacity-[0.85] h-[96px]" />
+
+
+
+                        <StyledView className="absolute self-center">
+                            <StyledIcon className="self-center text-center font-custom text-white" name="hammer" size={25} color="white" />
+                            <StyledText className="self-center text-center font-custom text-white text-2xl">SOON</StyledText>
+                        </StyledView>
+
+                    </StyledView>
+
+                    <StyledText className="absolute self-center text-center top-14 font-custom text-white text-2xl">หมวดหมู่ค้นหา</StyledText>
+
+
+                </LinearGradient>
+
+
             </KeyboardAvoidingView>
-            <Navigation current="SearchCategory"/>
+            <Navigation current="SearchCategory" />
         </StyledView>
     );
 }
