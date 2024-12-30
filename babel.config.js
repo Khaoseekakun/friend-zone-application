ce092de90ff9a3aac671d17c9cd267af45cc021f
Khao Seekakun <emitstudio13829@gmail.com>
2024-12-30 17:18:54 +0700

    f

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,12 @@
-module.exports = function (api) {
+module.exports = function async (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo', '@babel/preset-typescript'],
     plugins: [
       'nativewind/babel',
       'react-native-reanimated/plugin',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      '@babel/plugin-proposal-optional-chaining'
     ],
   };
 };
